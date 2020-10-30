81cf1974f1e89d7b39a0382c88b04c5bb1f5ad13
Holson Mpangala <holsonm74@gmail.com>
2020-10-30 15:28:01 +0100

    Tags Update

--- news/application/views/partials/_tags.php
+++ news/application/views/partials/_tags.php
@@ -1,16 +1,16 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 
 <!--Partial: Tags-->
-<div class="widget-title">
-    <h4 class="title"><?php echo html_escape(trans("tags")); ?></h4>
+<div class="">
+    <h4 class="title "><?php echo html_escape(trans("tags")); ?></h4>
 </div>
 <div class="col-sm-12 widget-body">
     <div class="row">
-        <ul class="widget-list w-tag-list">
+        <ul class="list-unstyled tag-list">
             <!--List  tags-->
             <?php foreach ($this->tags as $item): ?>
                 <li>
-                    <a href="<?php echo lang_base_url() . 'tag/' . html_escape($item->tag_slug); ?>">
+                    <a href="<?php echo lang_base_url() . 'tag/' . html_escape($item->tag_slug); ?>" class="btn btn-s">
                         <?php echo html_escape($item->tag); ?>
                     </a>
                 </li>
