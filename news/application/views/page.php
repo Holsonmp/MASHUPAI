1e263e47a069e73b136bc2ba471071c430e02420
Holson Mpangala <holsonm74@gmail.com>
2020-11-01 17:43:44 +0100

    Update Page

--- news/application/views/page.php
+++ news/application/views/page.php
@@ -8,118 +8,30 @@
       </div>
       <!-- /.container -->
     </div>
-
+    </div>
     <!-- /.offcanvas-info -->
     <div class="wrapper light-wrapper">
-      <div class="container inner pt-80">
-        <div class="row">
-          <div class="col-lg-10 offset-lg-1">
-            <div class="blog classic-view boxed">
-              <div class="post mb-0">
-                <div class="box bg-white shadow">
-                  <div class="space40"></div>
-                  <div class="post-content">
-                    <div class="category text-center"><a href="#" class="badge badge-pill bg-hibiscus">Concept</a></div>
-                    <h2 class="post-title text-center">Ullamcorper Ridiculus Tristique Adipiscing</h2>
-                    <div class="meta text-center"><span class="date"><i class="jam jam-clock"></i>5 Jul 2018</span><span class="author"><i class="jam jam-user"></i><a href="#">By Snowflake</a></span><span class="comments"><i class="jam jam-message-alt"></i><a href="#">3 Comments</a></span></div>
-                    <p>Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur. Curabitur blandit tempus porttitor. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Nullam quis risus eget porta ac consectetur vestibulum.</p>
-                    <p>Donec sed odio dui consectetur adipiscing elit. Etiam adipiscing tincidunt elit, eu convallis felis suscipit ut. Phasellus rhoncus tincidunt auctor. Nullam eu sagittis mauris. Donec non dolor ac elit aliquam tincidunt at at sapien. Aenean tortor libero, condimentum ac laoreet vitae, varius tempor nisi. Duis non arcu vel lectus urna mollis ornare vel eu leo.</p>
-                    <div class="space30"></div>
-                    <blockquote class="icon">
-                      <p>Sed posuere consectetur est at lobortis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis mollis, est non commodo luctus, nisi erat porttitor ligula lacinia odio sem nec elit. Cras mattis consectetur purus.</p>
-                      <footer class="blockquote-footer">Very important person</footer>
-                    </blockquote>
-                    <div class="space30"></div>
-                    <h3>Sit Vulputate Bibendum Purus</h3>
-                    <p>Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Aenean lacinia bibendum nulla sed consectetur. Cras mattis consectetur purus sit amet fermentum. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vestibulum id ligula porta felis euismod semper. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
-                    <div class="space20"></div>
-                    <div class="post-gallery light-gallery-wrapper">
-                      <div class="tiles tiles-s">
-                        <div class="items row">
-                          <div class="item col-md-6">
-                            <figure class="overlay overlay1 rounded"><a href="style/images/art/bgi1-full.jpg" class="lightbox"><img src="style/images/art/bgi1.jpg" alt=""></a>
-                              <figcaption>
-                                <h5 class="from-top mb-0">View Larger</h5>
-                              </figcaption>
-                            </figure>
-                          </div>
-                          <!--/.item -->
-                          <div class="item col-md-6">
-                            <figure class="overlay overlay1 rounded"><a href="style/images/art/bgi2-full.jpg" class="lightbox"><img src="style/images/art/bgi2.jpg" alt=""></a>
-                              <figcaption>
-                                <h5 class="from-top mb-0">View Larger</h5>
-                              </figcaption>
-                            </figure>
-                          </div>
-                          <!--/.item -->
-                          <div class="item col-md-4">
-                            <figure class="overlay overlay1 rounded"><a href="style/images/art/bgi3-full.jpg" class="lightbox"><img src="style/images/art/bgi3.jpg" alt=""></a>
-                              <figcaption>
-                                <h5 class="from-top mb-0">View Larger</h5>
-                              </figcaption>
-                            </figure>
-                          </div>
-                          <!--/.item -->
-                          <div class="item col-md-4">
-                            <figure class="overlay overlay1 rounded"><a href="style/images/art/bgi4-full.jpg" class="lightbox"><img src="style/images/art/bgi4.jpg" alt=""></a>
-                              <figcaption>
-                                <h5 class="from-top mb-0">View Larger</h5>
-                              </figcaption>
-                            </figure>
-                          </div>
-                          <!--/.item -->
-                          <div class="item col-md-4">
-                            <figure class="overlay overlay1 rounded"><a href="style/images/art/bgi5-full.jpg" class="lightbox"><img src="style/images/art/bgi5.jpg" alt=""></a>
-                              <figcaption>
-                                <h5 class="from-top mb-0">View Larger</h5>
-                              </figcaption>
-                            </figure>
-                          </div>
-                          <!--/.item -->
+        <div class="container inner pt-80">
+            <div class="row">
+                <div class="col-lg-10 offset-lg-1">
+                    <div class="blog classic-view boxed">
+                         <div class="post mb-0">
+                            <div class="box bg-white">
+                                <div class="space40"></div>
+                                    <div class="post-content">
+                                        <p>
+                                            <?php echo $page->page_content; ?>
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        <!--/.items -->
-                      </div>
-                      <!--/.tiles -->
                     </div>
-                    <!--/.post-gallery -->
-                    <div class="clearfix"></div>
-                    <div class="space40"></div>
-                    <p>Cras mattis consectetur purus sit amet fermentum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Nullam id dolor id nibh ultricies vehicula ut id elit. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Cras mattis consectetur purus sit amet fermentum. Donec sed odio dui. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Cras mattis consectetur purus sit amet fermentum. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Vestibulum id ligula porta felis euismod semper. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet.</p>
-                    <div class="space10"></div>
-                    <div class="d-lg-flex justify-content-between align-items-center meta-footer">
-                      <ul class="list-unstyled tag-list">
-                        <li><a href="#" class="btn btn-s">Still Life</a></li>
-                        <li><a href="#" class="btn btn-s">Urban</a></li>
-                        <li><a href="#" class="btn btn-s">Nature</a></li>
-                      </ul>
-                      <div class="space20 d-lg-none"></div>
-                      <div class="d-flex align-items-center">
-                        <p class="pr-20 mb-0"><strong>Share on:</strong></p>
-                        <ul class="social social-mute">
-                          <li><a href="#"><i class="jam jam-facebook"></i></a></li>
-                          <li><a href="#"><i class="jam jam-twitter"></i></a></li>
-                          <li><a href="#"><i class="jam jam-pinterest"></i></a></li>
-                        </ul>
-                        <!-- /.social -->
-                      </div>
-                    </div>
-                  </div>
-                  <!-- /.post-content -->
                 </div>
-                <!-- /.box -->
-              </div>
-
             </div>
-            <!-- /.blog -->
-          </div>
-          <!-- /column -->
         </div>
-        <!-- /.row -->
-      </div>
-      <!-- /.container -->
     </div>
-    <!-- /.wrapper -->    
-<!-- Section: main -->
+
 <section id="main">
     <div class="container">
         <div class="row">
