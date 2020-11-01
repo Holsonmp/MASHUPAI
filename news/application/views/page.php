045346dea0480429d60b01dc2319fe65130880ef
Holson Mpangala <holsonm74@gmail.com>
2020-11-01 17:35:28 +0100

    Page UPDATE

--- news/application/views/page.php
+++ news/application/views/page.php
@@ -1,13 +1,231 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 <h1 class="title-index"><?php echo html_escape($page->title); ?></h1>
 <div class="wrapper light-wrapper page-title-wrapper">
-<div class="space100"></div>
+    <div class="space100"></div>
       <div class="container inner text-center">
         <h1 class="page-title"><?php echo html_escape($page->title); ?></h1>
         <p class="lead">Aenean lacinia bibendum nulla sed consectetur</p>
       </div>
       <!-- /.container -->
     </div>
+
+    <!-- /.offcanvas-info -->
+    <div class="wrapper light-wrapper">
+      <div class="container inner pt-80">
+        <div class="row">
+          <div class="col-lg-10 offset-lg-1">
+            <div class="blog classic-view boxed">
+              <div class="post mb-0">
+                <div class="box bg-white shadow">
+                  <div class="space40"></div>
+                  <div class="post-content">
+                    <div class="category text-center"><a href="#" class="badge badge-pill bg-hibiscus">Concept</a></div>
+                    <h2 class="post-title text-center">Ullamcorper Ridiculus Tristique Adipiscing</h2>
+                    <div class="meta text-center"><span class="date"><i class="jam jam-clock"></i>5 Jul 2018</span><span class="author"><i class="jam jam-user"></i><a href="#">By Snowflake</a></span><span class="comments"><i class="jam jam-message-alt"></i><a href="#">3 Comments</a></span></div>
+                    <p>Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur. Curabitur blandit tempus porttitor. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Nullam quis risus eget porta ac consectetur vestibulum.</p>
+                    <p>Donec sed odio dui consectetur adipiscing elit. Etiam adipiscing tincidunt elit, eu convallis felis suscipit ut. Phasellus rhoncus tincidunt auctor. Nullam eu sagittis mauris. Donec non dolor ac elit aliquam tincidunt at at sapien. Aenean tortor libero, condimentum ac laoreet vitae, varius tempor nisi. Duis non arcu vel lectus urna mollis ornare vel eu leo.</p>
+                    <div class="space30"></div>
+                    <blockquote class="icon">
+                      <p>Sed posuere consectetur est at lobortis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis mollis, est non commodo luctus, nisi erat porttitor ligula lacinia odio sem nec elit. Cras mattis consectetur purus.</p>
+                      <footer class="blockquote-footer">Very important person</footer>
+                    </blockquote>
+                    <div class="space30"></div>
+                    <h3>Sit Vulputate Bibendum Purus</h3>
+                    <p>Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Aenean lacinia bibendum nulla sed consectetur. Cras mattis consectetur purus sit amet fermentum. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vestibulum id ligula porta felis euismod semper. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
+                    <div class="space20"></div>
+                    <div class="post-gallery light-gallery-wrapper">
+                      <div class="tiles tiles-s">
+                        <div class="items row">
+                          <div class="item col-md-6">
+                            <figure class="overlay overlay1 rounded"><a href="style/images/art/bgi1-full.jpg" class="lightbox"><img src="style/images/art/bgi1.jpg" alt=""></a>
+                              <figcaption>
+                                <h5 class="from-top mb-0">View Larger</h5>
+                              </figcaption>
+                            </figure>
+                          </div>
+                          <!--/.item -->
+                          <div class="item col-md-6">
+                            <figure class="overlay overlay1 rounded"><a href="style/images/art/bgi2-full.jpg" class="lightbox"><img src="style/images/art/bgi2.jpg" alt=""></a>
+                              <figcaption>
+                                <h5 class="from-top mb-0">View Larger</h5>
+                              </figcaption>
+                            </figure>
+                          </div>
+                          <!--/.item -->
+                          <div class="item col-md-4">
+                            <figure class="overlay overlay1 rounded"><a href="style/images/art/bgi3-full.jpg" class="lightbox"><img src="style/images/art/bgi3.jpg" alt=""></a>
+                              <figcaption>
+                                <h5 class="from-top mb-0">View Larger</h5>
+                              </figcaption>
+                            </figure>
+                          </div>
+                          <!--/.item -->
+                          <div class="item col-md-4">
+                            <figure class="overlay overlay1 rounded"><a href="style/images/art/bgi4-full.jpg" class="lightbox"><img src="style/images/art/bgi4.jpg" alt=""></a>
+                              <figcaption>
+                                <h5 class="from-top mb-0">View Larger</h5>
+                              </figcaption>
+                            </figure>
+                          </div>
+                          <!--/.item -->
+                          <div class="item col-md-4">
+                            <figure class="overlay overlay1 rounded"><a href="style/images/art/bgi5-full.jpg" class="lightbox"><img src="style/images/art/bgi5.jpg" alt=""></a>
+                              <figcaption>
+                                <h5 class="from-top mb-0">View Larger</h5>
+                              </figcaption>
+                            </figure>
+                          </div>
+                          <!--/.item -->
+                        </div>
+                        <!--/.items -->
+                      </div>
+                      <!--/.tiles -->
+                    </div>
+                    <!--/.post-gallery -->
+                    <div class="clearfix"></div>
+                    <div class="space40"></div>
+                    <p>Cras mattis consectetur purus sit amet fermentum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Nullam id dolor id nibh ultricies vehicula ut id elit. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Cras mattis consectetur purus sit amet fermentum. Donec sed odio dui. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Cras mattis consectetur purus sit amet fermentum. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Vestibulum id ligula porta felis euismod semper. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet.</p>
+                    <div class="space10"></div>
+                    <div class="d-lg-flex justify-content-between align-items-center meta-footer">
+                      <ul class="list-unstyled tag-list">
+                        <li><a href="#" class="btn btn-s">Still Life</a></li>
+                        <li><a href="#" class="btn btn-s">Urban</a></li>
+                        <li><a href="#" class="btn btn-s">Nature</a></li>
+                      </ul>
+                      <div class="space20 d-lg-none"></div>
+                      <div class="d-flex align-items-center">
+                        <p class="pr-20 mb-0"><strong>Share on:</strong></p>
+                        <ul class="social social-mute">
+                          <li><a href="#"><i class="jam jam-facebook"></i></a></li>
+                          <li><a href="#"><i class="jam jam-twitter"></i></a></li>
+                          <li><a href="#"><i class="jam jam-pinterest"></i></a></li>
+                        </ul>
+                        <!-- /.social -->
+                      </div>
+                    </div>
+                  </div>
+                  <!-- /.post-content -->
+                </div>
+                <!-- /.box -->
+              </div>
+              <!-- /.post -->
+              <div class="space50"></div>
+              <div class="box bg-white shadow">
+                <div class="row">
+                  <div class="col-md-4">
+                    <figure class="rounded"><img alt="" src="style/images/art/about.jpg" /></figure>
+                  </div>
+                  <!--/column -->
+                  <div class="col-md-8">
+                    <h4>About the Author</h4>
+                    <p class="mb-10">Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Maecenas faucibus mollis interdum. Fusce dapibus, tellus ac. Maecenas faucibus mollis interdum. Praesent commodo.</p>
+                    <ul class="social">
+                      <li><a href="#"><i class="jam jam-twitter"></i></a></li>
+                      <li><a href="#"><i class="jam jam-facebook"></i></a></li>
+                      <li><a href="#"><i class="jam jam-pinterest"></i></a></li>
+                      <li><a href="#"><i class="jam jam-vimeo"></i></a></li>
+                      <li><a href="#"><i class="jam jam-instagram"></i></a></li>
+                    </ul>
+                  </div>
+                  <!--/column -->
+                </div>
+                <!--/.row -->
+              </div>
+              <!-- /.box -->
+              <div class="space50"></div>
+              <div class="box bg-white shadow">
+                <div id="comments">
+                  <h3>5 Comments</h3>
+                  <ol id="singlecomments" class="commentlist">
+                    <li>
+                      <div class="message">
+                        <figure class="user rounded"><img alt="" src="style/images/art/u1.jpg" /></figure>
+                        <div class="message-inner">
+                          <h6 class="comment-author"><a href="#">Connor Gibson</a></h6>
+                          <p>Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Sed posuere consectetur est at lobortis. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Vestibulum id ligula porta felis euismod semper.</p>
+                          <div class="meta"> <span class="date">January 14, 2019</span><span class="reply"><a href="#">Reply</a></span> </div>
+                        </div>
+                      </div>
+                    </li>
+                    <li>
+                      <div class="message">
+                        <figure class="user rounded"><img alt="" src="style/images/art/u2.jpg" /></figure>
+                        <div class="message-inner">
+                          <h6 class="comment-author"><a href="#">Nikolas Brooten</a></h6>
+                          <p>Quisque tristique tincidunt metus non aliquam. Quisque ac risus sit amet quam sollicitudin vestibulum vitae malesuada libero. Mauris magna elit, suscipit non ornare et, blandit a tellus. Pellentesque dignissim ornare elit, quis mattis eros.</p>
+                          <div class="meta"> <span class="date">February 21, 2019</span><span class="reply"><a href="#">Reply</a></span> </div>
+                        </div>
+                      </div>
+                      <ul class="children">
+                        <li class="bypostauthor">
+                          <div class="message">
+                            <figure class="user rounded"><img alt="" src="style/images/art/u3.jpg" /></figure>
+                            <div class="message-inner bypostauthor">
+                              <h6 class="comment-author"><a href="#">Pearce Frye</a></h6>
+                              <p>Cras mattis consectetur purus sit amet fermentum. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Etiam porta sem malesuada magna mollis euismod. Maecenas sed diam eget risus varius blandit non.</p>
+                              <div class="meta"> <span class="date">February 22, 2019</span><span class="reply"><a href="#">Reply</a></span> </div>
+                            </div>
+                          </div>
+                          <ul class="children">
+                            <li>
+                              <div class="message">
+                                <figure class="user rounded"><img alt="" src="style/images/art/u2.jpg" /></figure>
+                                <div class="message-inner">
+                                  <h6 class="comment-author"><a href="#">Nikolas Brooten</a></h6>
+                                  <p>Nullam id dolor id nibh ultricies vehicula ut id. Cras mattis consectetur purus sit amet fermentum. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.</p>
+                                  <div class="meta"> <span class="date">April 4, 2019</span><span class="reply"><a href="#">Reply</a></span> </div>
+                                </div>
+                              </div>
+                            </li>
+                          </ul>
+                        </li>
+                      </ul>
+                    </li>
+                    <li>
+                      <div class="message">
+                        <figure class="user rounded"><img alt="" src="style/images/art/u4.jpg" /></figure>
+                        <div class="message-inner">
+                          <h6 class="comment-author"><a href="#">Lou Bloxham</a></h6>
+                          <p>Sed posuere consectetur est at lobortis. Vestibulum id ligula porta felis euismod semper. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+                          <div class="meta"> <span class="date">May 03, 2019</span><span class="reply"><a href="#">Reply</a></span> </div>
+                        </div>
+                      </div>
+                    </li>
+                  </ol>
+                </div>
+                <!-- /#comments -->
+                <div class="space80"></div>
+                <h3>Would you like to share your thoughts?</h3>
+                <p>Your email address will not be published. Required fields are marked *</p>
+                <div class="space20"></div>
+                <form class="comment-form">
+                  <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Name*">
+                  </div>
+                  <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Email*">
+                  </div>
+                  <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Website">
+                  </div>
+                  <div class="form-group">
+                    <textarea name="textarea" class="form-control" rows="5" placeholder="Enter your comment here..."></textarea>
+                  </div>
+                  <button type="submit" class="btn">Submit</button>
+                </form>
+                <!-- /.comment-form -->
+              </div>
+              <!-- /.box -->
+            </div>
+            <!-- /.blog -->
+          </div>
+          <!-- /column -->
+        </div>
+        <!-- /.row -->
+      </div>
+      <!-- /.container -->
+    </div>
+    <!-- /.wrapper -->    
 <!-- Section: main -->
 <section id="main">
     <div class="container">
