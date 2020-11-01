80b1d88a91a15e4ae851495273f49ee59e02fdd7
Holson Mpangala <holsonm74@gmail.com>
2020-11-01 17:45:14 +0100

    Update Page

--- news/application/views/page.php
+++ news/application/views/page.php
@@ -17,7 +17,6 @@
                     <div class="blog classic-view boxed">
                          <div class="post mb-0">
                             <div class="box bg-white">
-                                <div class="space40"></div>
                                     <div class="post-content">
                                         <p>
                                             <?php echo $page->page_content; ?>
