0e7c314ff5f576e4ff7261fd61bedd43768551a3
Holson Mpangala <holsonm74@gmail.com>
2020-11-02 16:53:39 +0100

    Update Faqs Pages

--- news/application/views/faq.php
+++ news/application/views/faq.php
@@ -1,3 +1,91 @@
 <?php
     //
 ?>
+    <!-- /.wrapper -->
+    <div class="wrapper light-wrapper">
+      <div class="container inner">
+        <h2 class="title-color color-gray text-center">What We Do?</h2>
+        <h3 class="display-3 text-center">The full service we are offering is specifically <br class="d-none d-lg-block" />designed to meet your business needs.</h3>
+        <div class="space40"></div>
+        <div class="row gutter-50">
+          <div class="col-md-6 col-lg-4">
+            <div class="d-flex flex-row justify-content-center">
+              <div>
+                <div class="icon icon-blob icon-blob-rose color-rose mr-25"> <i class="icofont-headphone-alt"></i> </div>
+              </div>
+              <div>
+                <h5>24/7 Support</h5>
+                <p>Nulla vitae elit libero, a pharetra augue. Donec id elit non mi porta gravida at eget metus. Cras justo.</p>
+              </div>
+            </div>
+          </div>
+          <!--/column -->
+          <div class="col-md-6 col-lg-4">
+            <div class="d-flex flex-row justify-content-center">
+              <div>
+                <div class="icon icon-blob icon-blob-blue color-blue mr-25"> <i class="icofont-safety"></i> </div>
+              </div>
+              <div>
+                <h5>Secure Payments</h5>
+                <p>Nulla vitae elit libero, a pharetra augue. Donec id elit non mi porta gravida at eget metus. Cras justo.</p>
+              </div>
+            </div>
+          </div>
+          <!--/column -->
+          <div class="space20 d-none d-md-block d-lg-none"></div>
+          <div class="col-md-6 col-lg-4">
+            <div class="d-flex flex-row justify-content-center">
+              <div>
+                <div class="icon icon-blob icon-blob-green color-green mr-25"> <i class="icofont-refresh"></i> </div>
+              </div>
+              <div>
+                <h5>Daily Updates</h5>
+                <p>Nulla vitae elit libero, a pharetra augue. Donec id elit non mi porta gravida at eget metus. Cras justo.</p>
+              </div>
+            </div>
+          </div>
+          <!--/column -->
+          <div class="space20 d-none d-lg-block"></div>
+          <div class="col-md-6 col-lg-4">
+            <div class="d-flex flex-row justify-content-center">
+              <div>
+                <div class="icon icon-blob icon-blob-purple color-purple mr-25"> <i class="icofont-chart-arrows-axis"></i> </div>
+              </div>
+              <div>
+                <h5>Research & Marketing</h5>
+                <p>Nulla vitae elit libero, a pharetra augue. Donec id elit non mi porta gravida at eget metus. Cras justo.</p>
+              </div>
+            </div>
+          </div>
+          <!--/column -->
+          <div class="space20 d-none d-md-block d-lg-none"></div>
+          <div class="col-md-6 col-lg-4">
+            <div class="d-flex flex-row justify-content-center">
+              <div>
+                <div class="icon icon-blob icon-blob-orange color-orange mr-25"> <i class="icofont-gear"></i> </div>
+              </div>
+              <div>
+                <h5>System Integration</h5>
+                <p>Nulla vitae elit libero, a pharetra augue. Donec id elit non mi porta gravida at eget metus. Cras justo.</p>
+              </div>
+            </div>
+          </div>
+          <!--/column -->
+          <div class="col-md-6 col-lg-4">
+            <div class="d-flex flex-row justify-content-center">
+              <div>
+                <div class="icon icon-blob icon-blob-violet color-violet mr-25"> <i class="icofont-earth"></i> </div>
+              </div>
+              <div>
+                <h5>SEO Service</h5>
+                <p>Nulla vitae elit libero, a pharetra augue. Donec id elit non mi porta gravida at eget metus. Cras justo.</p>
+              </div>
+            </div>
+          </div>
+          <!--/column -->
+        </div>
+        <!--/.row -->
+      </div>
+      <!-- /.container -->
+    </div>
+    <!-- /.wrapper -->
